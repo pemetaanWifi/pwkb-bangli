7f31e058a506853996529a22805ae0a1e18b4c83
pemetaanWifi <108091422+pemetaanWifi@users.noreply.github.com>
2022-06-28 19:04:46 +0800

    Update koneksi.php

--- MOBILE/Admin/koneksi.php
+++ MOBILE/Admin/koneksi.php
@@ -1,7 +1,7 @@
 <?php
-define('HOST', 'localhost');
-define('USER', 'root');
-define('PASS', '');
+define('HOST', 'sql6.freesqldatabase.com');
+define('USER', 'sql6501749');
+define('PASS', '3LSA9zjYQi');
 define('DB', 'sql6501749');
 $conn = new mysqli(HOST, USER, PASS, DB);
 if($conn==false):
