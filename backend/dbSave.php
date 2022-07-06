0169fcbccc3872adc9a1135d7c68ef74825388a9
pemetaanWifi <108091422+pemetaanWifi@users.noreply.github.com>
2022-07-06 20:27:30 +0800

    Update dbSave.php

--- backend/dbSave.php
+++ backend/dbSave.php
@@ -9,17 +9,17 @@
     
   
     if($conn == true){
-        $sql_macaddr = "SELECT * FROM tbjaringan WHERE macAddress LIKE '$macAddress'";
+        $sql_macaddr = "SELECT * FROM tbjaringan WHERE macaddress LIKE '$macAddress'";
         $macaddr_check = pg_query($conn, $sql_macaddr);
         if($macAddress == ""){
             echo "This an empty data";
         }        
         else if(pg_num_rows($macaddr_check) > 0){
-            $sql_level = "SELECT level FROM tbjaringan WHERE macAddress LIKE '$macAddress'";
+            $sql_level = "SELECT level FROM tbjaringan WHERE macaddress LIKE '$macAddress'";
             $level_check = pg_query($conn, $sql_level);
             $level_check_fetch = pg_fetch_array($level_check);
             if($level >= $level_check_fetch[0]){
-                $sql_register = "UPDATE tbJaringan SET level='$level',location='$location' WHERE macAddress LIKE '$macAddress'";
+                $sql_register = "UPDATE tbjaringan SET level='$level',location='$location' WHERE macaddress LIKE '$macAddress'";
                 if(pg_query($conn,$sql_register)){
                     echo "Data diperbarui";
                 }else{
@@ -31,7 +31,7 @@
             }
         }
         else{
-            $sql_register = "INSERT INTO tbjaringan (macAddress,ssid,level,frequency,capability,location) VALUES ('$macAddress','$ssid','$level','$frequency','$capability','$location')";
+            $sql_register = "INSERT INTO tbjaringan (macaddress,ssid,level,frequency,capability,location) VALUES ('$macAddress','$ssid','$level','$frequency','$capability','$location')";
             if(pg_query($conn,$sql_register)){
                 echo "Data ditambahkan";
             }else{
