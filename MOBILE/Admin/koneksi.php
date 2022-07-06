919219483a41cc0680a699e1e7049cdad949e16f
pemetaanWifi <108091422+pemetaanWifi@users.noreply.github.com>
2022-07-06 20:23:29 +0800

    Update koneksi.php

--- MOBILE/Admin/koneksi.php
+++ MOBILE/Admin/koneksi.php
@@ -1,9 +1,9 @@
 <?php
-define('HOST', 'sql6.freesqldatabase.com');
-define('USER', 'sql6501749');
-define('PASS', '3LSA9zjYQi');
-define('DB', 'sql6501749');
-$conn = new mysqli(HOST, USER, PASS, DB);
+$db_name = "d13gbsqvfk72d4";
+$username = "tqzwwxdkhvujwb";
+$password = "0125b49d9b9c0297b96c86cc66b606951c7b6b8aba427a169150c493bb0d7344";
+$servename = "ec2-54-227-248-71.compute-1.amazonaws.com";
+$conn = pg_connect("host=$servename port=5432 dbname=$db_name user=$username password=$password");
 if($conn==false):
     die("gagal melakukan koneksi :" .$conn->connect_error());
 endif;
