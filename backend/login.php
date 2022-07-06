f146ec1d7ff5bc10ff6ef1be75588373c60b9335
pemetaanWifi <108091422+pemetaanWifi@users.noreply.github.com>
2022-07-06 19:22:04 +0800

    Update login.php

--- backend/login.php
+++ backend/login.php
@@ -1,8 +1,9 @@
-<?php                                                                                                                               require "conn.php";
+<?php                                                                                                                               
+require "conn.php";
     $username = isset($_POST["username"]) ? $_POST["username"] : "";
     $password = isset($_POST["password"]) ? $_POST["password"] : "";
     if($conn){
-        $sqlLogin = "SELECT * FROM tbLogin WHERE username LIKE '$username' AND password LIKE '$password'";
+        $sqlLogin = "SELECT * FROM tblogin WHERE username LIKE '$username' AND password LIKE '$password'";
         $loginQuery = mysqli_query($conn,$sqlLogin);
         if(mysqli_num_rows($loginQuery) > 0){
             echo "Login Berhasil";
