af89d8fa2fb1cb7588f6b4bb5edbf46b204b3e88
pemetaanWifi <108091422+pemetaanWifi@users.noreply.github.com>
2022-07-06 19:40:44 +0800

    Update conn.php

--- backend/conn.php
+++ backend/conn.php
@@ -3,5 +3,5 @@
     $username = "tqzwwxdkhvujwb";
     $password = "0125b49d9b9c0297b96c86cc66b606951c7b6b8aba427a169150c493bb0d7344";
     $servename = "ec2-54-227-248-71.compute-1.amazonaws.com";
-    $conn = mysqli_connect($servename ,$username ,$password ,$db_name );
+    $conn = pg_connect($servename ,$username ,$password ,$db_name );
 ?>
