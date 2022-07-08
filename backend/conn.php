a8502252822a154ca16c6d1028be67403a7c132d
pemetaanWifi <108091422+pemetaanWifi@users.noreply.github.com>
2022-07-08 14:19:17 +0800

    Update conn.php

--- backend/conn.php
+++ backend/conn.php
@@ -2,6 +2,6 @@
     $db_name = "d13gbsqvfk72d4";
     $username = "tqzwwxdkhvujwb";
     $password = "b4ce4732e95bc55e5064a017e4d235be1f6274cdaa9673eee50c95fd86ced7a9";
-    $servename = "ec2-54-227-248-71.compute-1.amazonaws.com";
+    $servename = "ec2-34-239-241-121.compute-1.amazonaws.com";
     $conn = pg_connect("host=$servename port=5432 dbname=$db_name user=$username password=$password");
 ?>
