74e93e58f9fd8924020698ef7df85f2de8e13ff0
pemetaanWifi <108091422+pemetaanWifi@users.noreply.github.com>
2022-07-08 16:57:50 +0800

    Update conn.php

--- conn.php
+++ conn.php
@@ -4,10 +4,10 @@
     $password = "b4ce4732e95bc55e5064a017e4d235be1f6274cdaa9673eee50c95fd86ced7a9";
     $servename = "ec2-34-239-241-121.compute-1.amazonaws.com";
     $conn = pg_connect("host=$servename port=5432 dbname=$db_name user=$username password=$password");
-    $dbopts = parse_url(getenv('DATABASE_URL'));
+    $dbopts = parse_url(getenv('HEROKU_POSTGRESQL_BRONZE_URL'));
     echo $dbopts;
     echo "hello";
-    $dbopts = parse_url(getenv('DATABASE_URL'));
+    $dbopts = parse_url(getenv('HEROKU_POSTGRESQL_BRONZE_URL'));
     $app->register(new Csanquer\Silex\PdoServiceProvider\Provider\PDOServiceProvider('pdo'),
                    array(
                     'pdo.server' => array(
