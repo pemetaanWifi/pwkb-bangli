e6d58149b3261e97b7bcab00ff3e9a1c6303b41a
pemetaanWifi <108091422+pemetaanWifi@users.noreply.github.com>
2022-07-08 17:03:54 +0800

    Update conn.php

--- conn.php
+++ conn.php
@@ -21,4 +21,13 @@
                    )
     );
 
+    $db = (function(){
+        $parts = (parse_url(getenv('DATABASE_URL') ?: 'postgresql://username:password@localhost:5432/your_database_name_here'));
+        extract($parts);
+        $path = ltrim($path, "/");
+        return new PDO("pgsql:host={$host};port={$port};dbname={$path}", $user, $pass);
+    })();
+    
+    echo $db;
+
 ?>
