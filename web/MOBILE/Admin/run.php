cbf5f5f2a5e269d315be399adc6676717907af8c
pemetaanWifi <108091422+pemetaanWifi@users.noreply.github.com>
2022-07-11 19:21:39 +0800

    Update run.php

--- web/MOBILE/Admin/run.php
+++ web/MOBILE/Admin/run.php
@@ -3,6 +3,8 @@
     header("Content-Disposition: attachment; filename=Data.xls");
     header("Pragma: no-cache");
     header("Expires: 0");
+
+    require_once '../../koneksi.php';
 ?>
 <!DOCTYPE html>
 <head>
@@ -16,12 +18,12 @@
                 <th>Level</th>
                 <th>Frequency</th>
                 <th>Jenis Sandi</th>
+                <th>Terakhir Dipindai</th>
             </tr>
         </thead>
         <tbody>
             <?php
-                $mysqli = pg_connect("host=ec2-54-227-248-71.compute-1.amazonaws.com port=5432 dbname=d13gbsqvfk72d4 user=tqzwwxdkhvujwb password=0125b49d9b9c0297b96c86cc66b606951c7b6b8aba427a169150c493bb0d7344");
-                $jaringan = pg_query($mysqli, "select * from tbjaringan");
+                $jaringan = pg_query($conn, "select * from tbjaringan");
                 while($row = pg_fetch_array($jaringan))
                 {
                     echo'<tr>
@@ -30,6 +32,7 @@
                     <td>'.$row['level'].'</td>
                     <td>'.$row['frequency'].'</td>
                     <td>'.$row['capability'].'</td>
+                    <td>'.$row['date'].'</td>
                     </tr>';
                 }
                 
