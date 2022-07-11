f56bf610a9c38be570b56770612c93ff8fe0c1c7
pemetaanWifi <108091422+pemetaanWifi@users.noreply.github.com>
2022-07-11 19:59:26 +0800

    Update conn.php

--- web/conn.php
+++ web/conn.php
@@ -5,18 +5,10 @@
 //     $servename = "ec2-34-239-241-121.compute-1.amazonaws.com";
 //     $conn = pg_connect("host=$servename port=5432 dbname=$db_name user=$username password=$password");
 
-    $db = parse_url(getenv("postgres://yttgpfdezcpmwh:b5a8771ca470e873cf1c62ffbd60638d2645c51d0c0355eaec6214d99b42785d@ec2-34-233-115-14.compute-1.amazonaws.com:5432/dblt24t1fid74q"));
+$conn = pg_connect(getenv("postgres://yttgpfdezcpmwh:b5a8771ca470e873cf1c62ffbd60638d2645c51d0c0355eaec6214d99b42785d@ec2-34-233-115-14.compute-1.amazonaws.com:5432/dblt24t1fid74q"));
 
-    $pdo = new PDO("pgsql:" . sprintf(
-        "host=%s;port=%s;user=%s;password=%s;dbname=%s",
-        $db["host"],
-        $db["port"],
-        $db["user"],
-        $db["pass"],
-        ltrim($db["path"], "/")
-    ));
-echo $db;
-echo $pdo;
+echo $conn;
+
 echo "hello";
 
 ?>
