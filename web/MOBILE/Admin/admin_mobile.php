29f24a11327a90dff1d31b180882e5cb21795103
pemetaanWifi <108091422+pemetaanWifi@users.noreply.github.com>
2022-07-18 18:52:29 +0800

    Update admin_mobile.php

--- web/MOBILE/Admin/admin_mobile.php
+++ web/MOBILE/Admin/admin_mobile.php
@@ -160,8 +160,25 @@
                             <img src='/gambar/xls.png' width='30' /> Export
                         </a>
                     </p>
-                    <button type="submit" class="btn btn-md btn-danger" name="proses">Hapus</button>
-                    <span class="text-danger"><?php echo $jar_err; ?></span>
+                    <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">Hapus</button> 
+                    <span class="text-danger"><?php echo $jar_err; ?></span> 
+                    <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                        <div class="modal-dialog">
+                            <div class="modal-content">
+                                <div class="modal-header">
+                                    <h5 class="modal-title" id="exampleModalLabel">Yakin ingin manghapus file ini?</h5>
+                                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                </div>
+                                <div class="modal-body">
+                                Data akan di hapus secara permanan!
+                                </div>
+                                <div class="modal-footer">
+                                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                                    <button type="submit" class="btn btn-primary" name="proses">Hapus</button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
             </div>
         </br>
     </div>
