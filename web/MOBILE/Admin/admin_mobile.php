3414b72b22a87475a67632cec63df60bb70d7f1e
pemetaanWifi <108091422+pemetaanWifi@users.noreply.github.com>
2022-07-19 17:08:26 +0800

    Update admin_mobile.php

--- web/MOBILE/Admin/admin_mobile.php
+++ web/MOBILE/Admin/admin_mobile.php
@@ -166,15 +166,15 @@
                         <div class="modal-dialog">
                             <div class="modal-content">
                                 <div class="modal-header">
-                                    <h5 class="modal-title" id="exampleModalLabel">Yakin ingin manghapus file ini?</h5>
+                                    <h5 class="modal-title" id="exampleModalLabel">Yakin ingin manghapus data ini?</h5>
                                     <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                                 </div>
                                 <div class="modal-body">
-                                Data akan di hapus secara permanan!
+                                Data akan di hapus secara permanen!
                                 </div>
                                 <div class="modal-footer">
                                     <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                                    <button type="submit" class="btn btn-primary" name="proses">Hapus</button>
+                                    <button type="submit" class="btn btn-danger" name="proses">Hapus</button>
                                 </div>
                             </div>
                         </div>
