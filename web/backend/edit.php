fab430a9559f0b884a551cf1635b5abd8b6b3672
pemetaanWifi <108091422+pemetaanWifi@users.noreply.github.com>
2022-10-07 17:30:27 +0800

    Update edit.php

--- web/backend/edit.php
+++ web/backend/edit.php
@@ -1,13 +1,19 @@
 <?php
     require "../conn.php";
     if($conn == true){
-      $sql_register = "UPDATE tbjaringan SET frequency='5' WHERE frequency LIKE '5GHz'";
-      if(pg_query($conn,$sql_register)){
-          echo "Data diperbarui";
-      }else{
-          echo "Gagal memperbarui data";
-      }
-    }
+//       $sql_register = "UPDATE tbjaringan SET frequency='5' WHERE frequency LIKE '5GHz'";
+        
+//       $sql_register = "UPDATE tbjaringan SET frequency='5' WHERE frequency LIKE '5GHz'";
+      $jaringan = pg_query($conn, "select * from tbjaringan");
+      while($row = pg_fetch_array($jaringan)){
+            echo".$row['date'].";
+        }
+//       if(pg_query($conn,$sql_register)){
+//           echo "Data diperbarui";
+//       }else{
+//           echo "Gagal memperbarui data";
+//       }
+//     }
     else{
         echo "Connection Error";
     }
