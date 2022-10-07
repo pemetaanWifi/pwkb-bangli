adef2972205841e6ab9cd068e59623e660bf099a
pemetaanWifi <108091422+pemetaanWifi@users.noreply.github.com>
2022-10-07 18:39:19 +0800

    Update edit.php

--- web/backend/edit.php
+++ web/backend/edit.php
@@ -9,7 +9,7 @@
           $default_dt = $row['date'];
           $dt = date("Y-m-d h:i:s", strtotime($row['date']));
           if (str_contains($default_dt, 'pm')){
-              $dt = date("Y-m-d h:i:s", strtotime($dt)+43200);
+              $dt = date("Y-m-d h:i:s", strtotime('+24 hours', $dt));
               echo "12Hour: ". $default_dt. "  ||  24Hour: ". $dt. "<br>";
           }
           else{
