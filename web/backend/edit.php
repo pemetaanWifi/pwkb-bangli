b15bdef014d40ebb43a881e968a635fec8d869ba
pemetaanWifi <108091422+pemetaanWifi@users.noreply.github.com>
2022-10-07 20:00:13 +0800

    Update edit.php

--- web/backend/edit.php
+++ web/backend/edit.php
@@ -9,7 +9,7 @@
           $dt = date("Y-m-d G:i:s", strtotime($default_dt));
           echo "12Hours: ". $default_dt. " || 24Hours: ". $dt."<br>";
          
-          $sql_register = "UPDATE tbjaringan SET date='$dt' WHERE frequency LIKE '$default_dt'";
+          $sql_register = "UPDATE tbjaringan SET date='$dt' WHERE date LIKE '$default_dt'";
       if(pg_query($conn,$sql_register)){
           echo "Data diperbarui";
       }else{
