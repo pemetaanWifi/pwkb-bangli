b2446b39517cee9261182a0ac87b6bdb08fc7257
pemetaanWifi <108091422+pemetaanWifi@users.noreply.github.com>
2022-10-07 19:44:57 +0800

    Update edit.php

--- web/backend/edit.php
+++ web/backend/edit.php
@@ -9,7 +9,7 @@
           $default_dt = $row['date'];
 //           $dt = date("Y-m-d G:i:s", strtotime($default_dt));
           echo "12Hours: ". $default_dt;
-              . " || 24Hours: ". $dt."<br>";
+//               . " || 24Hours: ". $dt."<br>";
 //       if(pg_query($conn,$sql_register)){
 //           echo "Data diperbarui";
 //       }else{
