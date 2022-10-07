dc1b0b6779e2076c5f68d00f153f92e8c7f04f9f
pemetaanWifi <108091422+pemetaanWifi@users.noreply.github.com>
2022-10-07 20:48:10 +0800

    Update edit.php

--- web/backend/edit.php
+++ web/backend/edit.php
@@ -1,24 +1,25 @@
 <?php
-    require "../conn.php";
-    if($conn == true){
-//       $sql_register = "UPDATE tbjaringan SET frequency='5' WHERE frequency LIKE '5GHz'";
+//     require "../conn.php";
+//     if($conn == true){
+// //       $sql_register = "UPDATE tbjaringan SET frequency='5' WHERE frequency LIKE '5GHz'";
        
-      $jaringan = pg_query($conn, "select date from tbjaringan");
-      while($row = pg_fetch_array($jaringan)){
-          $default_dt = $row['date'];
-          $dt = date("Y-m-d G:i:s", strtotime($default_dt));
-          echo "12Hours: ". $default_dt. " || 24Hours: ". $dt."<br>";
+//       $jaringan = pg_query($conn, "select date from tbjaringan");
+//       while($row = pg_fetch_array($jaringan)){
+//           $default_dt = $row['date'];
+//           $dt = date("Y-m-d G:i:s", strtotime($default_dt));
+//           echo "12Hours: ". $default_dt. " || 24Hours: ". $dt."<br>";
          
-          $sql_register = "UPDATE tbjaringan SET date='$dt' WHERE date LIKE '$default_dt'";
-      if(pg_query($conn,$sql_register)){
-          echo "Data diperbarui";
-      }else{
-          echo "Gagal memperbarui data";
-      }
+//           $sql_register = "UPDATE tbjaringan SET date='$dt' WHERE date LIKE '$default_dt'";
+//       if(pg_query($conn,$sql_register)){
+//           echo "Data diperbarui";
+//       }else{
+//           echo "Gagal memperbarui data";
+//       }
           
-      }    
-    }
-    else{
-        echo "Connection Error";
-    }
+//       }    
+//     }
+//     else{
+//         echo "Connection Error";
+//     }
+DUMMY ONLY
 ?>
