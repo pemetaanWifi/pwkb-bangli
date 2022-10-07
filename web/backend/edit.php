4dd4bbf58647a6ae4cf6bdab860a08803373d7c2
pemetaanWifi <108091422+pemetaanWifi@users.noreply.github.com>
2022-10-07 17:53:06 +0800

    Update edit.php

--- web/backend/edit.php
+++ web/backend/edit.php
@@ -6,7 +6,7 @@
 //       $sql_register = "UPDATE tbjaringan SET frequency='5' WHERE frequency LIKE '5GHz'";
       $jaringan = pg_query($conn, "select date from tbjaringan");
       while($row = pg_fetch_array($jaringan)){
-            echo "date time: ". $row['date']. "  ||  24Hour: ". date("Y-m-d h:i:s", strtotime("$row['date']"). "<br>";
+            echo "date time: ". $row['date']. "  ||  24Hour: ". date("Y-m-d h:i:s", strtotime($row['date']);. "<br>";
         }
 //       if(pg_query($conn,$sql_register)){
 //           echo "Data diperbarui";
